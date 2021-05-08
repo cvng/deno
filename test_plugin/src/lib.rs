9a777d30493a42e14ee2b0acaa92a3af78ec1146
Cedric Vangout <mail@cvng.dev>
2021-05-08 16:50:37 +0400

    try args with plugin ops

--- test_plugin/src/lib.rs
+++ test_plugin/src/lib.rs
@@ -36,6 +36,7 @@ pub fn init() -> Extension {
 #[derive(Debug, Deserialize)]
 struct TestArgs {
   val: String,
+  map: std::collections::HashMap<String, String>, // TODO: comment here
 }
 
 fn op_test_sync(
